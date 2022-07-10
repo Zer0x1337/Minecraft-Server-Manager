d04d1be680c141cc0373762b19ad3af07c3186d1
k0xyy <76451333+k0xyy@users.noreply.github.com>
2022-07-10 15:45:15 -0400

    Update start.py
    Added Windows Support!

--- start.py
+++ start.py
@@ -3,10 +3,17 @@ import sys
 from sys import platform
 from subprocess import call
 
-os.system('clear')
+if platform == "linux" or platform == "linux2":
+    os.system('clear')
+elif platform == "win32":
+    os.system('cls')
 
-terminal_title = "Made by: k0xyy on GitHub"
-print(f'\33]0;{terminal_title}\a', end='', flush=True)
+if platform == "linux" or platform == "linux2":
+    terminal_title = "Made by: k0xyy on GitHub"
+    print(f'\33]0;{terminal_title}\a', end='', flush=True)
+elif platform == "win32":
+    os.system('title Made by: k0xyy on GitHub')
+
 
 print("Simple and 1337 AF Minecraft Server Launcher\n")
 print("   [1] Launch Existing Minecraft Server With Ngrok\n")
@@ -14,46 +21,77 @@ print("   [2] Launch Existing Minecraft Server\n")
 option = int(input(">>> "))
 
 if option == 1:
-    os.system('clear')
+    if platform == "linux" or platform == "linux2":
+        os.system('clear')
+    elif platform == "win32":
+        os.system('cls')
 
     mem_option_ngrok = str(input("\n   How Much Memory(Mb)?\n\n>>> "))
     mem_option_ngrok_check = str(input("\n   Are you sure you want to give this server " + mem_option_ngrok + "Mb of Memory? (Y/n)\n\n>>> "))
 
     if mem_option_ngrok_check == "y" or "Y":
-        os.system('clear')
+
+        if platform == "linux" or platform == "linux2":
+            os.system('clear')
+        elif platform == "win32":
+            os.system('cls')
 
         call(["gnome-terminal", "-x", "sh", "-c", "sudo ngrok tcp 25565; bash"])
-        terminal_title2 = "MC Server"
-        print(f'\33]0;{terminal_title2}\a', end='', flush=True)
+
+        if platform == "linux" or platform == "linux2":
+            terminal_title2 = "MC Server"
+            print(f'\33]0;{terminal_title2}\a', end='', flush=True)
+        elif platform == "win32":
+            os.system('MC Server')
         os.system('sudo java -Xmx' + mem_option_ngrok + 'M -Xms' + mem_option_ngrok + 'M -jar server.jar nogui') 
     if mem_option_ngrok_check == "n" or "N":
-        os.system('clear')
+        if platform == "linux" or platform == "linux2":
+            os.system('clear')
+        elif platform == "win32":
+            os.system('cls')
 
         mem_option_ngrok = str(input("\n   How Much Memory(Mb)?\n\n>>> "))
 
         call(["gnome-terminal", "-x", "sh", "-c", "sudo ngrok tcp 25565; bash"])
-        terminal_title2 = "MC Server"
-        print(f'\33]0;{terminal_title2}\a', end='', flush=True)
-        os.system('sudo java -Xmx' + mem_option_ngrok + 'M -Xms' + mem_option_ngrok + 'M -jar server.jar nogui') 
-
+        if platform == "linux" or platform == "linux2":
+            terminal_title2 = "MC Server"
+            print(f'\33]0;{terminal_title2}\a', end='', flush=True)
+        elif platform == "win32":
+            os.system('MC Server')
+        os.system('sudo java -Xmx' + mem_option_ngrok + 'M -Xms' + mem_option_ngrok + 'M -jar server.jar nogui')
 if option == 2:
-    os.system('clear')
+    if platform == "linux" or platform == "linux2":
+        os.system('clear')
+    elif platform == "win32":
+        os.system('cls')
 
     mem_option = str(input("\n   How Much Memory(Mb)?\n\n>>> "))
     mem_option_check = str(input("\n   Are you sure you want to give this server " + mem_option + "Mb of Memory? (Y/n)\n\n>>> "))
 
     if mem_option_check == "y" or "Y":
-        os.system('clear')
+        if platform == "linux" or platform == "linux2":
+            os.system('clear')
+        elif platform == "win32":
+            os.system('cls')
 
-        terminal_title2 = "MC Server"
-        print(f'\33]0;{terminal_title2}\a', end='', flush=True)
+        if platform == "linux" or platform == "linux2":
+            terminal_title2 = "MC Server"
+            print(f'\33]0;{terminal_title2}\a', end='', flush=True)
+        elif platform == "win32":
+            os.system('MC Server')
         os.system('sudo java -Xmx' + mem_option + 'M -Xms' + mem_option + 'M -jar server.jar nogui')
     if mem_option_check == "n" or "N":
-        os.system('clear')
+        if platform == "linux" or platform == "linux2":
+            os.system('clear')
+        elif platform == "win32":
+            os.system('cls')
 
         mem_option = str(input("\n   How Much Memory(Mb)?\n\n>>> "))
-        terminal_title2 = "MC Server"
-        print(f'\33]0;{terminal_title2}\a', end='', flush=True)
+        if platform == "linux" or platform == "linux2":
+            terminal_title2 = "MC Server"
+            print(f'\33]0;{terminal_title2}\a', end='', flush=True)
+        elif platform == "win32":
+            os.system('MC Server')
         os.system('sudo java -Xmx' + mem_option + 'M -Xms' + mem_option + 'M -jar server.jar nogui')
 
 
